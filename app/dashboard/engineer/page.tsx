7d278d6684e70258c2ef7fbba1591cb167548339
youngchoi-arrieta <ychoi0917@gmail.com>
2026-03-16 09:33:17 +0900

    홈 화면 이름 교체2

--- app/dashboard/engineer/page.tsx
+++ app/dashboard/engineer/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { supabase } from '../../lib/supabase'
+import { supabase } from '../../../lib/supabase'
 import Link from 'next/link'
 
 type ExamSession = {
